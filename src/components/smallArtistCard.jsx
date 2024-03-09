645980c7d95f69a7c26db4eefcf8746badee26d2
Aoyama Kazuki <sekibun3109@gmail.com>
2024-03-09 19:46:11 +0900

    add: smallArtistCardの作成[wip]

---
+++ src/components/smallArtistCard.jsx
@@ -0,0 +1,25 @@
+import sampleBandImg from '../images/sampleband.png';
+import { Card, CardBody, Image, Stack, Text } from '@chakra-ui/react';
+
+const SmallArtistCard = () => {
+  return (
+    <Card direction={'row'} overflow="hidden" variant="outline">
+      <Image objectFit="contain" maxW={100} src={sampleBandImg} alt="band" />
+
+      <Stack>
+        <CardBody>
+          <Stack spacing="3">
+            <Text color="black" fontSize="ms" fontWeight="bold">
+              バンド名
+            </Text>
+            <Text color="gray" fontSize="sm">
+              ジャンル
+            </Text>
+          </Stack>
+        </CardBody>
+      </Stack>
+    </Card>
+  );
+};
+
+export { SmallArtistCard };
