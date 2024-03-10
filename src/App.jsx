6fb1d9e124782f5109d6da100b5b84024db4ea3f
honaka5 <honoprin411@gmail.com>
2024-03-10 11:14:02 +0900

    Boxで囲ってpaddingを８にしました

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { Box } from '@chakra-ui/react';
 import Header from './components/header';
 import { SmallArtistCard } from './components/smallArtistCard';
 function App() {
   return (
     <>
       <Header />
-      <SmallArtistCard />
+      <Box paddingX={8}><SmallArtistCard /></Box>
+      
     </>
   );
 }
