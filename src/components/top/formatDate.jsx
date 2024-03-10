1d3ab08aaa532336fa5b49b872ae9d9c77a4af9b
Aoyama Kazuki <sekibun3109@gmail.com>
2024-03-11 03:05:26 +0900

    rem: ファイル削除

--- src/components/top/formatDate.jsx
+++
@@ -1,9 +0,0 @@
-const FormatDate = ({ date }) => {
-  const yyyy = date.getFullYear();
-  const mm = ('00' + (date.getMonth() + 1)).slice(-2);
-  const dd = ('00' + date.getDate()).slice(-2);
-
-  return `${yyyy}年${mm}月${dd}日`;
-};
-
-export { FormatDate };
