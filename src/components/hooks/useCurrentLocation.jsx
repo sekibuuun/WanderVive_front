cff188993fd7a5aa814b66daf6101136bb520d6e
Aoyama Kazuki <sekibun3109@gmail.com>
2024-03-10 09:24:52 +0900

    fix: 初回レンダリング時に位置情報を取得

--- src/components/hooks/useCurrentLocation.jsx
+++ src/components/hooks/useCurrentLocation.jsx
@@ -1,21 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const useCurrentLocation = () => {
   const [userLocation, setUserLocation] = useState(null);
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        setUserLocation({ latitude, longitude });
-      },
-      (error) => {
-        console.error('Error getting user location:', error);
-      },
-    );
-  } else {
-    console.error('Geolocation is not supported by this browser.');
-  }
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ latitude, longitude });
+        },
+        (error) => {
+          console.error('Error getting user location:', error);
+        },
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  }, []);
 
   return userLocation;
 };
